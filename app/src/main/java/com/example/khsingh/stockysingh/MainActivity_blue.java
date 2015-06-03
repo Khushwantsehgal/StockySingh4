e85f1dffca5872d3898ea28c0e415dc831a0657d
Khushwantsehgal <khushwant.sehgal@gmail.com>
2015-06-03 13:28:16 +0530

    Added the title bar

--- app/src/main/java/com/example/khsingh/stockysingh/MainActivity_blue.java
+++ app/src/main/java/com/example/khsingh/stockysingh/MainActivity_blue.java
@@ -2,14 +2,25 @@ package com.example.khsingh.stockysingh;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 /**
  * Created by khsingh on 5/31/15.
  */
-public class MainActivity_blue extends Activity {
+public class MainActivity_blue extends ActionBarActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_blue);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
